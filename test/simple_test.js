dbba00ddedc05bb942ede21fa64649859ac40ace
Stacey Vetzal <stacey@vetzal.com>
2017-07-28 07:30:31 -0400

    a stub example

--- test/simple_test.js
+++ test/simple_test.js
@@ -1,3 +1,4 @@
+const sinon = require('sinon');
 const expect = require('chai').expect;
 const doSomeWork = require('../src/do_some_work');
 
@@ -9,7 +10,18 @@ describe('doSomeWork', () => {
   });
 
   it('should call the nacho thing', () => {
-    // oh oh
+    // oh oh, let's play...
+
+    const fakeNacho = {
+      doSomething: () => {
+      }
+    };
+
+    sinon.spy(fakeNacho, 'doSomething');
+
+    fakeNacho.doSomething(1,2,3);
+
+    expect(fakeNacho.doSomething.withArgs(1,2,3).calledOnce).to.be.true;
   });
 });
 
